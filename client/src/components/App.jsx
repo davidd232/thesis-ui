63e35aab96b11d17f9a2a36256aeadad49cf4082
David Darley <daviddar232@gmail.com>
2018-02-01 10:50:03 -0800

    [ops] - socket server connected to App component.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
+import io from 'socket.io-client/dist/socket.io.js';
 
 class App extends Component {
   constructor() {
     super();
+    this.state = {
+      socket: null
+    }
+  }
+  componentWillMount() {
+    this.socket = io('http://localhost:4155', {
+      query: {
+        roomId: 'hey'
+      }
+    });
+    this.setState({ socket: this.socket });
   }
   render() {
     return (
       <div>
         <h1>Hello from React</h1>
+        <input type="text"></input>
+        <button type="submit">submit</button>
       </div>
     )
   }
